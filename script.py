903821eab19ccf8a0c0128e2186f504137d1e450
Jayden Lind <jaydenlind@gmail.com>
2020-09-30 15:29:45 +1000

    got login message

--- script.py
+++ script.py
@@ -3,7 +3,10 @@ import paramiko
 client = paramiko.SSHClient()
 client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 client.connect('10.0.80.4', username='admin', password='')
-stdin, stdout, stderr = client.exec_command('get interface eth1')
+stdin, stdout, stderr = client.exec_command('')
+stdin.write('\n')
+stdin.write('admin\n')
+stdin.write('\n')
 for line in stdout:
     print('... ' + line.strip('\n'))
 client.close()
