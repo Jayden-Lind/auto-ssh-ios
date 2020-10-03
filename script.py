2bc246078748d6ab6325d79178eb303257972a55
Jayden Lind <jaydenlind@gmail.com>
2020-10-03 16:52:20 +1000

    working now, testing script with proper output

--- script.py
+++ script.py
@@ -1,13 +1,20 @@
-import base64
 import paramiko
+import time
+f = open("credentials", "r")
+password = (f.read())
 client = paramiko.SSHClient()
 client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-client.connect('10.0.80.4', username='admin', password='')
-stdin, stdout, stderr = client.exec_command('\n')
-print(stdout)
-stdin.write('admin\n')
-print(stdout)
-stdin.write('\n')
-for line in stdout:
-    print(line.strip('\n'))
-client.close()
+client.connect('10.0.80.4', port=22, username='', password='')
+
+channel = client.get_transport().open_session()
+channel.invoke_shell()
+
+while channel.recv_ready():
+    channel.recv(1024)
+
+print(password)
+channel.sendall("admin\n")
+channel.sendall(password + "\n")
+time.sleep(1)
+channel.sendall("set interface eth1 type access untag 150\n")
+print(channel.recv(1024))
